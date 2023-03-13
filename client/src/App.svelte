7cc395367f4ef9ea3f88949b13d691c10801eba8
JMC50 <minchan50@naver.com>
2023-03-14 02:23:42 +0900

    로그인 감지

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
   import Login from "./lib/Login.svelte";
-    import Main from "./lib/Main.svelte";
-    import Register from "./lib/Register.svelte";
-    import Weather from "./lib/Weather.svelte";
+  import Main from "./lib/Main.svelte";
+  import Register from "./lib/Register.svelte";
+  import Weather from "./lib/Weather.svelte";
+  import { useAuth } from "./store/auth";
 
 
   let hash = location.hash
-    if(!hash) {
-        hash = '#login'
-    }
-    let hashes:string[] = [];
-    const pages = ["#login"];
+  if(!hash) {
+      hash = '#login'
+  }
+  let hashes:string[] = [];
+  const pages = ["#login"];
 
-    $:{
-        location.hash = hash;
-        hashes = hash.match(/\#\w+/g);
-        // console.log(hash, hashes);
-    }
+  if($useAuth.id == ''){
+    hash = '#login';
+  }
 
-    const hashChangeFun = () => {
-        const arr:string[] = hash.match(/\#\w+/g);
-        for(let i of arr){
-            if(pages.includes(i)){
-                if(arr.indexOf(i) == arr.length){
-                  hash = location.hash;
-                }
-                continue
-            }
-        }
-    }
+  $:{
+      location.hash = hash;
+      hashes = hash.match(/\#\w+/g);
+      // console.log(hash, hashes);
+  }
+
+  const hashChangeFun = () => {
+      const arr:string[] = hash.match(/\#\w+/g);
+      for(let i of arr){
+          if(pages.includes(i)){
+              if(arr.indexOf(i) == arr.length){
+                hash = location.hash;
+              }
+              continue
+          }
+      }
+  }
 </script>
 <svelte:window on:hashchange={hashChangeFun}></svelte:window>
 <main>
